c728927e8c0f083d6d51785a47432617695c399b
Adithya Mallya <SlaYpoint>
2021-03-21 20:55:45 +0530

    Scripting done

--- bananaTalk/app.js
+++ bananaTalk/app.js
@@ -2,9 +2,29 @@ var btnTranslate = document.querySelector("#btnTranslate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" ;
+
+// var serverURL = "	https://api.funtranslations.com/translate/minion.json";
+
+
+function getTranslationURL(text){
+    return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(err){
+    console.log("error has occured",err);
+    alert("some issue with the server: try after sometime.");
+}
+
 btnTranslate.addEventListener("click", function clickEventHandler() {
     console.log("Clicked !");
-    output.innerText = txtInput.value;
+    
+    fetch(getTranslationURL(txtInput.value))
+    .then(response => response.json())
+    .then(json => {
+        outputDiv.innerHTML = json.contents.translated})
+    .catch(errorHandler);
+
 })
 
 
