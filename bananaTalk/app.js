a1b5dc1681b109ee959e9bc13e5e0c7b736ddbd4
Adithya Mallya <SlaYpoint>
2021-03-22 20:27:44 +0530

    changed id to class element

--- bananaTalk/app.js
+++ bananaTalk/app.js
@@ -1,10 +1,8 @@
 var btnTranslate = document.querySelector("#btnTranslate");
-var txtInput = document.querySelector("#txt-input");
+var txtInput = document.querySelector(".content");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" ;
-
-// var serverURL = "	https://api.funtranslations.com/translate/minion.json";
+var serverURL = "	https://api.funtranslations.com/translate/minion.json";
 
 
 function getTranslationURL(text){
